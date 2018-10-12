1ee98a4d0984344f70aa4351d2dc7a42353b32cf
Andrew Kairys <andrewkairys@gmail.com>
2018-10-12 18:28:05 +0000

    automatically backed up by learn

--- spec/controllers/repositories_controller_spec.rb
+++ spec/controllers/repositories_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe RepositoriesController, type: :controller do
+RSpec.describe RepositoriesController, type: :controller do 1
 
   describe "github_search" do
 
